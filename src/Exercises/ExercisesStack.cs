6947bd6810419fc86ce88f5c92a995f8f6c2521f
Jimmy Ritter <jimmy.ritter@playngo.com>
2023-11-17 15:02:44 +0100

    Initial commit

---
+++ src/Exercises/ExercisesStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Exercises
+{
+    public class ExercisesStack : Stack
+    {
+        internal ExercisesStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}

00cb817c16a9412ac63f90b30aab2ad4b6f2fcb4
Jimmy Ritter <jimmy.ritter@playngo.com>
2023-11-17 16:32:38 +0100

    CDK exercise

---
+++ aws-cdk-hands-on/src/AwsCdkHandsOn/AwsCdkHandsOnStack.cs
@@ -0,0 +1,26 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.S3;
+using Constructs;
+
+namespace AwsCdkHandsOn
+{
+	public class AwsCdkHandsOnStack : Stack
+	{
+		internal AwsCdkHandsOnStack(Construct scope, string id, string stage, IStackProps props = null) : base(scope,
+			id, props)
+		{
+			_ = new Bucket(this, $"cdkhandson-jimmy-{stage}", new BucketProps
+			{
+				Cors = new ICorsRule[]
+				{
+					new CorsRule
+					{
+						AllowedMethods = new[] { HttpMethods.GET },
+						AllowedOrigins = new[] { "https://www.playngo.com" },
+						AllowedHeaders = new[] { "*" }
+					}
+				}
+			});
+		}
+	}
+}
---
+++ aws-cdk-hands-on/src/AwsCdkHandsOn/Program.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AwsCdkHandsOn
+{
+	sealed class Program
+	{
+		public static void Main(string[] args)
+		{
+			var app = new App();
+			new AwsCdkHandsOnStack(app, $"cdkhandson-jimmy-{Stages.Dev}", Stages.Dev, new StackProps());
+			new AwsCdkHandsOnStack(app, $"cdkhandson-jimmy-{Stages.Qa}", Stages.Qa, new StackProps());
+			app.Synth();
+		}
+	}
+
+	public static class Stages
+	{
+		public static string Dev => "Dev";
+		public static string Qa => "QA";
+	}
+}
